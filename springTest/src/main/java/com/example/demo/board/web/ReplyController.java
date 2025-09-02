9e59ae1fab1cf32073b3f9f55064a9b146e15f63
LeeWoongJae <dsa00340@naver.com>
2025-09-03 01:02:41 +0900

    update and delete 구현

--- springTest/src/main/java/com/example/demo/board/web/ReplyController.java
+++ springTest/src/main/java/com/example/demo/board/web/ReplyController.java
@@ -1,8 +1,12 @@
 package com.example.demo.board.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,25 +30,35 @@ public class ReplyController {
 	
 	// 수정
 	@PutMapping("/reply/{rno}")
-	public String update() {
-		return "";
+	public int update(@RequestBody ReplyVO vo, @PathVariable("rno")Long rno) {
+		ReplyVO reply = new ReplyVO();
+		reply.setRno(rno);
+		reply.setReply(vo.getReply());
+		int result = replyMapper.update(reply);
+		return result;
 	}
 	
 	
 	// 삭제
 	@DeleteMapping("/reply/{rno}")
-	public String delete() {
-		return "";
+	public int delete(@PathVariable("rno")Long rno) {
+		int result = replyMapper.delete(rno);
+		return result;
 	}
 	
 	// 전체조회
-	@GetMapping("board/{bno}/reply")
-	public String list() {
-		return "";
+	@GetMapping("/board/{bno}/reply")
+	public List<ReplyVO> list(ReplyVO reply , Model model) {
+		reply.setBno(1L);
+		reply.setNextPage(10);
+		reply.setPrevPage(1);
+		List<ReplyVO> list = replyMapper.getList(reply);
+		model.addAttribute("reply",list);
+		return list;
 	}
 	
 	// 단건조회
-	@GetMapping("reply/{rno}")
+	@GetMapping("/reply/{rno}")
 	public String get() {
 		return "";
 	}
